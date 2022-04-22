1ce2ddf4e38c6c14b1fd9e68e9848e7d7424d452
孙忠祥 <szy110110@qq.com>
2022-04-22 16:16:18 +0800

    2022-04-22

--- push2github.sh
+++ push2github.sh
@@ -2,7 +2,8 @@ cd /home/ubuntu/code/backup_szx_life
 # 每次生成静态文件都会重复生成，所以每次都要重新删除
 #rm -rf *%*
 # 可选，由于镜像点和源站点没有区别，为了区别，我把我的头像文件给替换了，换成黄四郎的“糟了，我成替身了”，这样就能区别两个站点，也算是一个彩蛋了。
-cp erciyuan4.webp wp-content/uploads/2021/07/erciyuan4.webp 
+cp huangsilang.webp wp-content/uploads/2021/07/huangsilang.webp 
+sed -i s/erciyuan4.webp/huangsilang.webp/g `grep -rl erciyuan4.webp --include="*.html" ./`
 git add .
 # 提交，每次都是随机数
 git commit -m "$(date +%Y-%m-%d)"
